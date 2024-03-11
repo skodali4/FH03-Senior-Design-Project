a3b4ca7a94c81e51c0a4679488764894d96aa048
Nick Strohmeyer <142917077+nastroh22@users.noreply.github.com>
2024-03-11 15:47:20 -0500

    Update template.py
    more template updates

--- template.py
+++ template.py
@@ -59,6 +59,33 @@ def simulate():
         # or something like this, 20% and 80% are arbitrary ^^
     return solution_dist
 
+def another_simulate()
+    """ this is just another example of how we might construct the above ^"""
+
+    ## Some Dummy Inputs 
+    T = 10  # Time steps in advance when the user 
+    DATE = "01/01/2024" # Date migt be used to pull some real starting condtions
+    ETH_BANK = 5   # This is the Agent's starting Ethereum amount
+    Beta = 2.2     # This is the Agent's Collateral Ratio 
+    Supply = 1000  # "pull_dai_value_from( DATE )" -- might be a hypothetical supply 
+    Demand_Forecast = []*T  # This is a prediction T steps ahead 
+
+    vars = normal_risk(N)
+    betas = normal_betas(N,B)
+    shares = randomize_shares(N)
+    
+    agents = [DaiSpeculator(supply = shares[i]*Supply, beta = betas[i], 
+                                    var = vars[i]) for i in range(N)]
+    
+    for n,agent in enumerate(agents):
+        print("\nSpec Agent ", n)
+        print("Supply Share:" , agent.S)
+        print("Collateral Ratio:", agent.Beta)
+        print("Risk Tol: ", agent.Var)
+
+    return
+
+
 
 class DaiSpeculator:
     """{Description} This is a suggested structure to use for wrapping
